f08d6089544e438ed59ff406b651fe5a80000e34
zkx261124 <476445557.@qq.com>
2018-08-10 14:13:57 +0800

    firsit commit

---
+++ src/main/java/com/atguigu/HelloWorldMainApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldMainApplication {
+    public static void main(String[] args) {
+        // Spring应用启动起来
+        SpringApplication.run(HelloWorldMainApplication.class,args);
+    }
+
+}
